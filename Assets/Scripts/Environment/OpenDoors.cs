18b4d966a9e5fdfce1699c7a6a6418315df705c6
Aaron <WilsonWorldA@gmail.com>
2023-09-09 11:36:42 -0400

    Finished updating the doors and gates to be unlockable. They can now also open and close. Created new animations for closing doors and for an idle state for the button (allowing the button animation to be shown each time the button can be pressed). Updated the lights on the double door to show better on both sides. Added a lock light and lock sign. Added a texture to the signs and cards for Level Access Zero. Updated the ceiling lights to look better, provide a little more light, and reduced the range while adding an addition light to the top (which produces the shadows), and moved on to the middle (no shadows, more light). Added a keycard to the Demo scene and created a lamp light to indicate it's position.

--- Assets/Scripts/Environment/OpenDoors.cs
+++ Assets/Scripts/Environment/OpenDoors.cs
@@ -1,6 +1,7 @@
 /* Created by Wilson World Games, August 2022 */
 /* The Open Doors scripts creates a seting of horizontally opening doors that the player can activate once.  */
 
+using System.Collections;
 using UnityEngine;
 
 public class OpenDoors : Interactable
@@ -10,36 +11,48 @@ public class OpenDoors : Interactable
     public GameObject RightDoor;
     public GameObject DoorLight;
     public GameObject LockLight;
-    public Color RedLight;
-    public Color GreenLight;
     public Inventory PlayerInventoryRef;
-
     [SerializeField] int DoorCode = -2;
     [SerializeField] bool isDoubleDoor = true;
     [SerializeField] bool bLocked = false;
     bool bOpened = false;
+
+    [Header("Door Effects")]
+    public Color RedLight;
+    public Color GreenLight;
+    [SerializeField] AudioSource MoveSFX;
+
 
     private void Start()
     {
         if (isDoubleDoor && DoorLight) {
             DoorLight.GetComponent<MeshRenderer>().material.color = RedLight;
             DoorLight.transform.GetChild(0).GetComponent<Light>().color = RedLight;
+            DoorLight.transform.GetChild(1).GetComponent<Light>().color = RedLight;
         }
 
         if (bLocked && LockLight) {
             LockLight.GetComponent<MeshRenderer>().material.color = RedLight;
             LockLight.GetComponent <Light>().color = RedLight;
         }
+
+        if (bLocked == false && LockLight) {
+            LockLight.GetComponent<MeshRenderer>().material.color = GreenLight;
+            LockLight.GetComponent<Light>().color = GreenLight;
+        }
     }
 
     private void OnMouseOver()
     {
         ActivateDisplayUI();
-        SetInteractionText("Open Doors");
+        if (bOpened)
+            SetInteractionText("Close Doors");
+        else
+            SetInteractionText("Open Doors");
 
         if (Input.GetButtonDown("Interact") && Distance <= DistanceMax) {
             DeactivateDisplayUI();
-            Open();
+            OnDoorButtonPress();
         }
     }
 
@@ -48,37 +61,41 @@ public class OpenDoors : Interactable
         DeactivateDisplayUI();
     }
 
-    // Check if the door is locked and if the player has the key for it. Opens the doors if able.
+    // Check if the door is open or closed to perform the corresponding action
+    void OnDoorButtonPress()
+    {
+        PlayButtonFX();
+        StartCoroutine(ResetCollider());
+
+        if (bOpened)
+            Close();
+        else
+            Open();
+    }
+
+    // Checks 'Lock' status and 'Open' status to determine appropriate open action and sets the 'Open' state to true
     void Open()
     {
-        if (bLocked) {
-            foreach ( Key key in PlayerInventoryRef.m_Keys) {
-                if (key.KeyCode == DoorCode) {
-                    UpdateLockLight();
-                    bLocked = false;
-                    break;
-                }
-            }
-        }
+        CheckForLock();
 
         if (bLocked)
             return;
 
-        if (isDoubleDoor) {
-            this.GetComponent<BoxCollider>().enabled = false;
-            PlayOpenFX();
-            UpdateDoorLight();
-        }
-        else {
-            PlayOpenFX();
-        }
+        CheckOpenState();
+        bOpened = true;
+    }
+
+    // Checks 'Open' status to determine appropriate close action and sets the 'Open' state to false
+    void Close()
+    {
+        CheckOpenState();
+        bOpened = false;
     }
 
     // Plays the effects for opening the doors
     void PlayOpenFX()
     {
-        this.GetComponent<AudioSource>().Play();
-        this.GetComponent<Animator>().Play("ButtonPress");
+        PlayMoveSFX();
 
         if (isDoubleDoor) {
             LeftDoor.GetComponent<Animator>().Play("LeftDoorSlide");
@@ -89,11 +106,72 @@ public class OpenDoors : Interactable
         }
     }
 
-    // Changes the door light from red to green, showing it's been opened
+    // Plays the effects for closing the doors
+    void PlayCloseFX()
+    {
+        PlayMoveSFX();
+
+        if (isDoubleDoor) {
+            LeftDoor.GetComponent<Animator>().Play("LeftDoorReturn");
+            RightDoor.GetComponent<Animator>().Play("RightDoorReturn");
+        }
+        else {
+            LeftDoor.GetComponent<Animator>().Play("SingleDoorReturn");
+        }
+    }
+
+    // Turn the collider off for 2 seconds and turn it back on
+    IEnumerator ResetCollider()
+    {
+        this.GetComponent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(2.0f);
+        this.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    // Look through the keys in the player inventory for a match if the door is locked.
+    void CheckForLock()
+    {
+        if (bLocked == false)
+            return;
+
+        if (PlayerInventoryRef == null || PlayerInventoryRef.m_Keys.Count == 0)
+            return;
+
+        foreach (Key key in PlayerInventoryRef.m_Keys) {
+            if (key.KeyCode == DoorCode) {
+                UpdateLockLight();
+                bLocked = false;
+                return;
+            }
+        }
+    }
+
+    // Check if attached object is a gate or door and play appropriate opening effect
+    void CheckOpenState()
+    {
+        if (isDoubleDoor)
+            UpdateDoorLight();
+
+        if (bOpened)
+            PlayCloseFX();
+        else
+            PlayOpenFX();
+    }
+
+
+    // Changes the door light between red and green to show if it's currently open
     void UpdateDoorLight()
     {
-        DoorLight.GetComponent<MeshRenderer>().material.color = GreenLight;
-        DoorLight.transform.GetChild(0).GetComponent<Light>().color = GreenLight;
+        if (bOpened) {
+            DoorLight.GetComponent<MeshRenderer>().material.color = RedLight;
+            DoorLight.transform.GetChild(0).GetComponent<Light>().color = RedLight;
+            DoorLight.transform.GetChild(1).GetComponent<Light>().color = RedLight;
+        }
+        else {
+            DoorLight.GetComponent<MeshRenderer>().material.color = GreenLight;
+            DoorLight.transform.GetChild(0).GetComponent<Light>().color = GreenLight;
+            DoorLight.transform.GetChild(1).GetComponent<Light>().color = GreenLight;
+        }
     }
 
     // Changes the lock light from red to green, showing it's been unlocked
@@ -103,5 +181,25 @@ public class OpenDoors : Interactable
             LockLight.GetComponent<MeshRenderer>().material.color = GreenLight;
             LockLight.GetComponent<Light>().color = GreenLight;
         }
+    }
+
+    IEnumerator ReturnButtonToIdle()
+    {
+        yield return new WaitForSeconds(0.5f);
+        this.GetComponent<Animator>().Play("ButtonIdle");
+    }
+
+    // Plays the interaction sfx and press animation for the button
+    void PlayButtonFX()
+    {
+        this.GetComponent<AudioSource>().Play();
+        this.GetComponent<Animator>().Play("ButtonPress");
+        StartCoroutine(ReturnButtonToIdle());
+    }
+
+    void PlayMoveSFX()
+    {
+        if (MoveSFX)
+            MoveSFX.Play();
     }
 }
